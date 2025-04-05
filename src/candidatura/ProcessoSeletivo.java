66d7cf2c3a1afb3b851367c7c8c159bd29f7799b
Carlos Vinícyus <carlosvinicyuss@gmail.com>
2025-04-04 23:11:03 -0300

    imprimindo-lista-de-candidatos-selecionados

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -5,7 +5,21 @@ import java.util.concurrent.ThreadLocalRandom;
 public class ProcessoSeletivo {
 
     public static void main(String[] args) {
-        selecaoCandidatos();
+        imprimirSelecionados();
+    }
+
+    static void imprimirSelecionados() {
+        String[] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO"};
+        System.out.println("Imprimindo a lista de candidatos informando o índice do elemento");
+        for (int i = 0; i < candidatos.length; i++) {
+            System.out.println("O candidato de nº" + (i+1) + " é " + candidatos[i]);
+        }
+
+        System.out.println("Forma abreviada de interação for each");
+
+        for (String candidato : candidatos) {
+            System.out.println("O candidato selecionado foi " + candidato);
+        }
     }
 
     static void selecaoCandidatos() {
